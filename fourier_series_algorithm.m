eae5a6d59ee56793424797138089818d96c2adfd
Curran Robertson <100748551+curranjrobertson@users.noreply.github.com>
2023-03-31 16:52:13 -0600

    init

---
+++ fourier_series_algorithm.m
@@ -0,0 +1,26 @@
+% Curran Robertson
+% Fourier Series Expansion
+
+clear all;clc; close all
+
+x = @(t) cos(t).*sin(t) % function (change)
+w = 0.5 % omega (change)
+a0 = (w/pi)*integral(x, 0, 2*pi/w) % first coefficient
+xf = a0/2 % first value 
+
+N = 5 % iterations (change)
+i = 1 % init loop
+while i < N
+    syms t
+    ai = (w/pi)*int(x.*cos(i*w*t), [0, 2*pi/w]);
+    bi = (w/pi)*integral(@(t) x(t).*sin(i*w*t), 0, 2*pi/w);
+    xf = xf + ai*cos(i*w*t)+bi*sin(i*w*t);
+    i = i+1;
+    
+end
+
+pretty(xf)
+
+fplot(xf)
+figure
+fplot(x, 'r')
